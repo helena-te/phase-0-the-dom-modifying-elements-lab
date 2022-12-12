a5e3de07331b9156818757fd881ccd3036dbaff0
Helena Tenev <htenev14@gmail.com>
2022-12-11 22:40:16 -0500

    commit tree f385292f444c94c396b4b2dd7ffe7b5ac3b2d2ac

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Helena is the champion";
+document.body.append(newHeader);
+
+// const h1 = document.createElement("h1");
+// document.body.append(h1);
+// 
+//newHeader.append(h1);
